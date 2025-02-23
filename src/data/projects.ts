e0bf698e695c5405cb4d76bfd62d1017469f6db2
v1br <vibhorag2003@gmail.com>
2025-02-23 22:45:14 +0530

    chore: update projects data

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -3,6 +3,7 @@ export enum Languages {
   TYPESCRIPT = 'Typescript',
   PYTHON = 'Python',
   GO = 'Go',
+  RUST = 'Rust'
 }
 
 export const projects = [
@@ -15,47 +16,71 @@ export const projects = [
     authorLink: 'https://github.com/woaitsAryan',
   },
   {
-    projectTitle: 'regit',
-    projectDescription:
-      'A CLI tool written in Golang to rewrite Git histories.',
-    language: Languages.TYPESCRIPT,
-    builtBy: 'Aryan Bharti',
-    repoLink: 'https://github.com/woaitsAryan/regit',
-    authorLink: 'https://github.com/woaitsAryan',
-  },
-  {
     projectTitle: 'kustom',
-    projectDescription: 'Customizable Form Builder - Forms on steroids',
+    projectDescription: 'Customizable Form Builder - Forms on steroids.',
     language: Languages.TYPESCRIPT,
     builtBy: 'Keshav Aneja',
     repoLink: 'https://kustom.cc',
     authorLink: 'https://github.com/Keshav-Aneja',
   },
   {
+    projectTitle: 'on-my-way-up',
+    projectDescription: 'A phaserjs game where a frog leaps past tricky obstacles.',
+    language: Languages.JAVASCRIPT,
+    builtBy: 'Vibhor Agrawal',
+    repoLink: 'https://github.com/VibhorAgrawal2003/On-My-Way-Up',
+    authorLink: 'https://github.com/v1br',
+  },
+  {
+    projectTitle: 'fileshare',
+    projectDescription: 'a decentralized blockchain based file sharing system.',
+    language: Languages.TYPESCRIPT,
+    builtBy: 'Manas Laud',
+    repoLink: 'https://github.com/manaslaud/FileShare',
+    authorLink: 'https://github.com/manaslaud',
+  },
+  {
+    projectTitle: 'regit',
+    projectDescription:
+      'A CLI tool written in Golang to rewrite Git histories.',
+    language: Languages.GO,
+    builtBy: 'Aryan Bharti',
+    repoLink: 'https://github.com/woaitsAryan/regit',
+    authorLink: 'https://github.com/woaitsAryan',
+  },
+  {
+    projectTitle: 'csi-logger',
+    projectDescription:
+      'CSI\'s very own logger to keep track of running processes.',
+    language: Languages.GO,
+    builtBy: 'Aryan Bharti',
+    repoLink: 'https://github.com/csivitu/csi-logger',
+    authorLink: 'https://github.com/woaitsAryan',
+  },
+  {
+    projectTitle: 'lasertag-gravitas',
+    projectDescription: "The Backend for this year's Gravitas Lasertag.",
+    language: Languages.JAVASCRIPT,
+    builtBy: 'Yug Oswal',
+    repoLink: 'https://github.com/csivitu/lasertag-gravitas-csi',
+    authorLink: 'https://github.com/Yug-Oswal',
+  },
+  {
     projectTitle: 'ctf-challenges',
     projectDescription:
-      'An aggregation of the Capture The Flag challenges and write-ups for CSICTF 2020',
+      'An aggregation of the CTF challenges and write-ups for CSI-CTF-2020',
     language: Languages.JAVASCRIPT,
     builtBy: 'Rohan Mukherjee',
     repoLink: 'https://github.com/csivitu/ctf-challenges',
     authorLink: 'https://github.com/roerohan',
   },
   {
-    projectTitle: 'lasertag-gravitas',
-    projectDescription: "The Backend for this year's Gravitas Lasertag",
+    projectTitle: 'ccs-2025',
+    projectDescription: "The project repository for this website, built as a collaborative effort.",
     language: Languages.JAVASCRIPT,
     builtBy: 'Yug Oswal',
-    repoLink: 'https://github.com/csivitu/lasertag-gravitas-csi',
-    authorLink: '',
-  },
-  {
-    projectTitle: 'bl0b',
-    projectDescription:
-      'A discord bot to notify you about upcoming CTFs from CTFtime, and more.',
-    language: Languages.JAVASCRIPT,
-    builtBy: 'Rohan Mukherjee, Mannan Goyal',
-    repoLink: 'https://github.com/csivitu/bl0b',
-    authorLink: '',
+    repoLink: 'https://github.com/csivitu/ccs-2025',
+    authorLink: 'https://github.com/csivitu',
   },
   {
     projectTitle: 'code-executor',
@@ -64,7 +89,33 @@ export const projects = [
     language: Languages.TYPESCRIPT,
     builtBy: 'Ashikka Gupta, Rohan Mukherjee, Rahil Kabani',
     repoLink: 'https://github.com/csivitu/code-executor',
-    authorLink: '',
+    authorLink: 'https://github.com/csivitu',
+  },
+  {
+    projectTitle: 'frontfoot',
+    projectDescription: 'Friendly server bot, dice rolls, magic 8 ball & more!',
+    language: Languages.PYTHON,
+    builtBy: 'Vibhor Agrawal',
+    repoLink: 'https://github.com/v1br/Frontfoot-Discord-Bot',
+    authorLink: 'https://github.com/v1br',
+  },
+  {
+    projectTitle: 'bl0b',
+    projectDescription:
+      'A discord bot to notify about upcoming CTFs from CTFtime, and more.',
+    language: Languages.GO,
+    builtBy: 'Rohan Mukherjee, Mannan Goyal',
+    repoLink: 'https://github.com/csivitu/bl0b',
+    authorLink: 'https://github.com/csivitu',
+  },
+  {
+    projectTitle: 'pls.give',
+    projectDescription:
+      'A discord bot for shortening links and scheduling meetings.',
+    language: Languages.RUST,
+    builtBy: 'David Velho, Rishit Bansal',
+    repoLink: 'https://github.com/csivitu/pls.give',
+    authorLink: 'https://github.com/csivitu',
   },
   {
     projectTitle: 'evie',
@@ -72,6 +123,7 @@ export const projects = [
     language: Languages.JAVASCRIPT,
     builtBy: 'Sanjay Kumar Baskaran, Mannan Goyal',
     repoLink: 'https://github.com/csivitu/Evie-Frontend',
-    authorLink: '',
+    authorLink: 'https://github.com/csivitu',
   },
+
 ]
